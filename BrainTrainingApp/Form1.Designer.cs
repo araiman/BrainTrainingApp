6ec0965d0721af00e2435f7d16c06e8d106394f7
RyomaArai <RyomaArai@RyomaArai-no-MacBook-Air.local>
2015-05-01 13:24:14 +0900

    First commit

---
+++ BrainTrainingApp/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace BrainTrainingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.handPictureBox = new System.Windows.Forms.PictureBox();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.SecondsLabel = new System.Windows.Forms.Label();
+            this.QNumLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.winOrLoseLabel = new System.Windows.Forms.Label();
+            this.handStateTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.handPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // handPictureBox
+            // 
+            this.handPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("handPictureBox.Image")));
+            this.handPictureBox.Location = new System.Drawing.Point(257, 194);
+            this.handPictureBox.Name = "handPictureBox";
+            this.handPictureBox.Size = new System.Drawing.Size(440, 426);
+            this.handPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.handPictureBox.TabIndex = 0;
+            this.handPictureBox.TabStop = false;
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Font = new System.Drawing.Font("Segoe UI Symbol", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeLabel.Location = new System.Drawing.Point(162, 786);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(221, 106);
+            this.TimeLabel.TabIndex = 1;
+            this.TimeLabel.Text = "Time";
+            // 
+            // SecondsLabel
+            // 
+            this.SecondsLabel.AutoSize = true;
+            this.SecondsLabel.Font = new System.Drawing.Font("Segoe UI Symbol", 100.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SecondsLabel.Location = new System.Drawing.Point(438, 693);
+            this.SecondsLabel.Name = "SecondsLabel";
+            this.SecondsLabel.Size = new System.Drawing.Size(399, 221);
+            this.SecondsLabel.TabIndex = 2;
+            this.SecondsLabel.Text = "38.6";
+            // 
+            // QNumLabel
+            // 
+            this.QNumLabel.AutoSize = true;
+            this.QNumLabel.Font = new System.Drawing.Font("Segoe UI Symbol", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QNumLabel.Location = new System.Drawing.Point(13, 13);
+            this.QNumLabel.Name = "QNumLabel";
+            this.QNumLabel.Size = new System.Drawing.Size(133, 159);
+            this.QNumLabel.TabIndex = 3;
+            this.QNumLabel.Text = "1";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(433, 891);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click_1);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(895, 918);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 5;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // winOrLoseLabel
+            // 
+            this.winOrLoseLabel.AutoSize = true;
+            this.winOrLoseLabel.Font = new System.Drawing.Font("メイリオ", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.winOrLoseLabel.Location = new System.Drawing.Point(721, 14);
+            this.winOrLoseLabel.Name = "winOrLoseLabel";
+            this.winOrLoseLabel.Size = new System.Drawing.Size(195, 180);
+            this.winOrLoseLabel.TabIndex = 6;
+            this.winOrLoseLabel.Text = "勝";
+            // 
+            // handStateTimer
+            // 
+            //this.handStateTimer.Tick += new System.EventHandler(this.handStateTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 953);
+            this.Controls.Add(this.winOrLoseLabel);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.QNumLabel);
+            this.Controls.Add(this.SecondsLabel);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.handPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Loaded);
+            ((System.ComponentModel.ISupportInitialize)(this.handPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox handPictureBox;
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Label SecondsLabel;
+        private System.Windows.Forms.Label QNumLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Label winOrLoseLabel;
+        private System.Windows.Forms.Timer handStateTimer;
+    }
+}
+
---
+++ BrainTrainingApp/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Kinect;
+
+namespace BrainTrainingApp
+{
+    public partial class Form1 : Form
+    {
+        public int qNum;
+        public float answerTime = 0;
+        public HandState handState;
+
+        KinectSensor kinect;
+        BodyFrameReader bodyFrameReader;
+
+
+        Body[] bodies;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+
+        private void Form1_Loaded(object sender, EventArgs e)
+        {
+            try
+            {
+                kinect = KinectSensor.GetDefault();
+                if (kinect == null)
+                {
+                    throw new Exception("Kinectを開けません");
+                }
+
+                kinect.Open();
+
+                // Bodyを入れる配列を作る
+                bodies = new Body[kinect.BodyFrameSource.BodyCount];
+
+                // ボディーリーダーを開く
+                bodyFrameReader = kinect.BodyFrameSource.OpenReader();
+                bodyFrameReader.FrameArrived += bodyFrameReader_FrameArrived;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+        }
+
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (bodyFrameReader != null)
+            {
+                bodyFrameReader.Dispose();
+                bodyFrameReader = null;
+            }
+
+            if (kinect != null)
+            {
+                kinect.Close();
+                kinect = null;
+            }
+        }
+
+        // ボディの更新
+        private void UpdateBodyFrame(BodyFrameArrivedEventArgs e)
+        {
+            using (var bodyFrame = e.FrameReference.AcquireFrame())
+            {
+                if (bodyFrame == null)
+                {
+                    return;
+                }
+
+
+                bodyFrame.GetAndRefreshBodyData(bodies);
+                updateHandState(bodies);
+            }
+        }
+
+
+        //手の情報（グー・チョキ・パー）の更新
+        public void updateHandState(Body[] bodies)
+        {
+            foreach (var body in bodies.Where(b => b.IsTracked))
+            {
+                foreach (var joint in body.Joints)
+                {
+                    if (joint.Value.TrackingState == TrackingState.Tracked)
+                    {
+                        if (joint.Value.JointType == JointType.HandRight)
+                        {
+                            handState = body.HandRightState;
+
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+
+        //クリックイベント
+
+        private void startButton_Click_1(object sender, EventArgs e)
+        {
+
+
+            qNum = 1;
+
+            QNumLabel.Text = qNum.ToString();
+
+
+            foreach (var body in bodies.Where(b => b.IsTracked))
+            {
+                foreach (var joint in body.Joints)
+                {
+                    if (joint.Value.TrackingState == TrackingState.Tracked)
+                    {
+                        if (joint.Value.JointType == JointType.HandRight)
+                        {
+                            //decideWinOrLose();
+                            decideQuestion();
+
+                        }
+                        else if (joint.Value.TrackingState == TrackingState.Inferred)
+                        {
+                            MessageBox.Show("手の平をしっかりと、カメラに向けてください。");
+                        }
+
+                    }
+                }
+            }
+
+
+
+            // TimeSpan startTime = DateTime.Now.TimeOfDay;
+            //Console.Write(startTime);
+
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            qNum++;
+
+            QNumLabel.Text = qNum.ToString();
+
+
+            //ここはリファクタリングしたい
+            decideQuestion();
+            //decideWinOrLose();
+
+            if (qNum > 20)
+            {
+                // TimeSpan endTime = DateTime.Now.TimeOfDay;
+                //TimeSpan startTime;
+
+                //TimeSpan recordTime = endTime - startTime;
+
+                MessageBox.Show("Finish!!");
+                //MessageBox.Show(recordTime.ToString("ss"));
+
+
+
+            }
+        }
+
+
+        //出題する問題を決定する
+
+        private void decideQuestion()
+        {
+
+            Random nRandom = new System.Random();
+
+
+            float handNum = nRandom.Next(3);
+            double handNumFloor = Math.Floor(handNum);
+
+
+            if (handNumFloor == 0)
+            {
+                handPictureBox.Image = BrainTrainingApp.Properties.Resources.rock;
+                Console.Write(handState);
+
+                while (handState != HandState.Open)
+                {
+                    MessageBox.Show("外れ！");
+                }
+
+                //handStateTimer.Stop();
+                nextButton.PerformClick();
+
+            }
+            else if (handNumFloor == 1)
+            {
+                handPictureBox.Image = BrainTrainingApp.Properties.Resources.paper;
+                Console.Write(handState);
+
+                while (handState != HandState.Lasso)
+                {
+                    MessageBox.Show("外れ！");
+                }
+
+                handStateTimer.Stop();
+                nextButton.PerformClick();
+
+
+            }
+            else
+            {
+                handPictureBox.Image = BrainTrainingApp.Properties.Resources.scissors;
+                Console.Write(handState);
+
+                while (handState != HandState.Closed)
+                {
+                    MessageBox.Show("外れ！");
+                }
+
+                //handStateTimer.Stop();
+                nextButton.PerformClick();
+            }
+
+        }
+
+
+
+        /*
+        private void decideWinOrLose()
+        {
+            Random wOrLRandom = new System.Random();
+
+            float wOrLNum = wOrLRandom.Next(2);
+            double wOrLNumFloor = Math.Floor(wOrLNum);
+
+
+            if (wOrLNumFloor == 0)
+            {
+                winOrLoseLabel.Text = "勝";
+
+            }
+            else
+            {
+                winOrLoseLabel.Text = "負";
+            }
+        }
+        */
+
+
+        //身体が動くたびに、働くメソッド
+
+        void bodyFrameReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            UpdateBodyFrame(e);
+        }
+
+
+        
+
+        /*
+        private void updateHandStateTimer()
+        {
+            handStateTimer.Interval = 1000;
+            handStateTimer.Start();
+        }
+
+        private void handStateTimer_Tick(object sender, EventArgs e)
+        {
+            updateHandState();
+        }
+        */
+    }
+
+}
+
